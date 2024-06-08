5ed2693266cc8232f8f22714c219bf1848f37b87
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-06-08 02:22:21 -0300

    Removendo pedidos indesejados

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,9 @@
 import styleHeader from '@/styles/header.module.css';
 import Link from 'next/link';
 import { Button } from 'primereact/button';
+import hamburguesData from '@/products/hamburgues.json';
+import { useEffect, useState } from 'react';
+import { Dialog } from 'primereact/dialog';
 
 interface Pedido {
     id_produto: number;
@@ -8,24 +11,89 @@ interface Pedido {
     quantidade: number;
 }
 
+interface Hamburgues {
+    id: number;
+    name: string;
+    type: number;
+    description: string;
+    ingredients: string;
+    price: number;
+}
+
 interface HeaderProps {
     selectedProducts: Pedido[];
     totalPrice: number;
+    setSelectedProducts: (products: Pedido[]) => void; // Adicione esta linha
+    setTotalPrice: (price: number) => void;
 }
 
-export default function Header({ selectedProducts, totalPrice }: HeaderProps) {
-    const handleShowCart = () => {
-        console.log("Produtos selecionados:");
-        console.log(selectedProducts);
-        console.log("Preço total:", totalPrice);
+export default function Header({ selectedProducts, totalPrice, setSelectedProducts,setTotalPrice }: HeaderProps) {
+
+    useEffect(() => {
+        function handleClickOutside(event: MouseEvent) {
+            const dialogElement = document.querySelector('.p-dialog');
+            if (dialogElement && event.target instanceof Node && !dialogElement.contains(event.target)) {
+                closeDialog();
+            }
+        }
+
+        document.body.addEventListener('click', handleClickOutside);
+        return () => {
+            document.body.removeEventListener('click', handleClickOutside);
+        };
+    }, []);
+
+    const [visible, setVisible] = useState(false);
+    const hamburgues: Hamburgues[] = hamburguesData;
+
+    const openDialog = () => {
+        setVisible(true);
     };
+
+    const closeDialog = () => {
+        setVisible(false);
+    };
+
+    const handleShowCart = () => {
+        openDialog();
+    };
+
+    const handleRemoveProduct = (id_pedido: number, price: number, qtd: number) => {
+        /* console.log(id_pedido); */
+        const newSelectedProducts = [...selectedProducts];
+        newSelectedProducts.splice(id_pedido, 1);
+        setSelectedProducts(newSelectedProducts); 
+        setTotalPrice(totalPrice -= (price * qtd));
+    };
+
+    const handleFinalizeOrder = () => {
+       
+    };
+
     return (
         <header className={`${styleHeader.header}`}>
             <Link href="">
                 LOGO
-            </Link> 
+            </Link>
 
             <button onClick={handleShowCart}>Exibir Carrinho</button>
+
+            <Dialog visible={visible} style={{ width: '50vw', borderRadius: '20px', backgroundColor:'white', color:'black'}} onHide={closeDialog} modal>
+                <div>
+                    <h2>Seus Pedidos</h2>
+                    {selectedProducts.map((product, index) => {
+                        const burger = hamburgues.find(burger => burger.id === product.id_produto);
+                        return (
+                            <div key={index}>
+                                <p>Nome: {burger ? burger.name : 'Hambúrguer não encontrado'} Quantidade: {product.quantidade} Preço: {burger ? burger.price : '-'}</p>
+                                <button onClick={() => handleRemoveProduct(index, burger ? burger.price:0, product.quantidade)}>Remover</button>
+                            </div>
+                        );
+                    })}
+                    <h3>Preço Total: {totalPrice.toFixed(2)}</h3>
+                    <button onClick={handleFinalizeOrder}>Finalizar Pedido</button>
+                </div>
+            </Dialog>
         </header>
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,35 @@
-import Image from "next/image";
-import { Oswald } from "next/font/google";
-import Header from "@/components/Header";
-import Menu from "@/components/Menu";
-import { useState } from "react";
+import Image from 'next/image';
+import { Oswald } from 'next/font/google';
+import Header from '@/components/Header';
+import Menu from '@/components/Menu';
+import { useState } from 'react';
 
-const oswald = Oswald({ subsets: ["latin"] });
+const oswald = Oswald({ subsets: ['latin'] });
 
 interface Pedido {
-  id_produto: number;
-  type: number;
-  quantidade: number;
+    id_produto: number;
+    type: number;
+    quantidade: number;
 }
 
 export default function Home() {
-  const [selectedProducts, setSelectedProducts] = useState<Pedido[]>([]);
+    const [selectedProducts, setSelectedProducts] = useState<Pedido[]>([]);
+    const [totalPrice, setTotalPrice] = useState(0);
 
-  const [totalPrice, setTotalPrice] = useState(0);
-  return (
-    <main className={oswald.className}>
-      <Header selectedProducts={selectedProducts} totalPrice={totalPrice} />
-      <Menu selectedProducts={selectedProducts} setSelectedProducts={setSelectedProducts} setTotalPrice={setTotalPrice} totalPrice={0}/>
-    </main>
-  );
+    return (
+        <main className={oswald.className}>
+            <Header 
+                selectedProducts={selectedProducts} 
+                totalPrice={totalPrice} 
+                setTotalPrice={setTotalPrice}
+                setSelectedProducts={setSelectedProducts} 
+            />
+            <Menu 
+                selectedProducts={selectedProducts} 
+                setSelectedProducts={setSelectedProducts} 
+                setTotalPrice={setTotalPrice} 
+                totalPrice={totalPrice} 
+            />
+        </main>
+    );
 }
