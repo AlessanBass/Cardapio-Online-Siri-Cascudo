22d78306dd885e6c6b09b3c013fae067dd75c468
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-06-08 10:01:45 -0300

    Feedback adição do carrinho

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <title>Faça aqui o seu pedido!</title>
       <Head />
+      <link rel="icon" href="/logo.png" ></link>
+      <script src="https://kit.fontawesome.com/29712bfbae.js" crossOrigin="anonymous"></script>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
       <body>
         <Main />
         <NextScript />
