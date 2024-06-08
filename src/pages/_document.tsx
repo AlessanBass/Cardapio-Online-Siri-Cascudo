defd642e90fb119e653f2b9c3dc95b033ecff6c9
Alessandro Conceição Santos <alessanbaixista@gmail.com>
2024-06-08 13:57:31 -0300

    Correção script sincrono

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,24 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <title>Faça aqui o seu pedido!</title>
-      <Head />
-      <link rel="icon" href="/logo.png" ></link>
-      <script src="https://kit.fontawesome.com/29712bfbae.js" crossOrigin="anonymous"></script>
-      <link rel="preconnect" href="https://fonts.googleapis.com" />
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-      <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
+      <Head>
+        <title>Faça aqui o seu pedido!</title>
+        <link rel="icon" href="/logo.png" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet" />
+      </Head>
       <body>
         <Main />
         <NextScript />
+        <Script
+          src="https://kit.fontawesome.com/29712bfbae.js"
+          strategy="beforeInteractive"
+          crossOrigin="anonymous"
+        />
       </body>
     </Html>
   );
